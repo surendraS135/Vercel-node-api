1df30567f9a2c4ad94de0cdfe2f65ee508f06c6a
surendraS135 <110030942+surendraS135@users.noreply.github.com>
2024-05-16 15:37:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: 'ep-small-river-a4pf78lk-pooler.us-east-1.aws.neon.tech',
+    host: 'ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech',
   //  port: env.DB_PORT || '5432',
     user: 'default',
-    password: 'PEIXl7nrGfm4',
+    password: '9OPZaWocGtd4',
     database: 'verceldb',
   },
-  POSTGRES_URL: "postgres://default:PEIXl7nrGfm4@ep-small-river-a4pf78lk-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  POSTGRES_URL: "postgres://default:9OPZaWocGtd4@ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   listPerPage: 10,
   pool: {
 
