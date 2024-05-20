158bee04ca4414f08a78dd3327555ffa436e72d0
surendraS135 <110030942+surendraS135@users.noreply.github.com>
2024-05-20 10:55:25 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ const env = process.env;
 
 module.exports = {
   db: {
-    host: env.POSTGRES_HOST, //"ep-small-river-a4pf78lk-pooler.us-east-1.aws.neon.tech",
+    host: "ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech", //env.POSTGRES_HOST, //"ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech",
     //  port: env.DB_PORT || '5432',
-    user: env.POSTGRES_USER, //"default",
-    password: env.POSTGRES_PASSWORD, //"PEIXl7nrGfm4",
-    database: env.POSTGRES_DATABASE, //"verceldb",
+    user: "default", //env.POSTGRES_USER, //"default",
+    password: "9OPZaWocGtd4", //env.POSTGRES_PASSWORD, //"9OPZaWocGtd4",
+    database: "verceldb", //env.POSTGRES_DATABASE, //"verceldb",
   },
-  POSTGRES_URL: env.POSTGRES_URL,
-    //"postgres://default:PEIXl7nrGfm4@ep-small-river-a4pf78lk-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  POSTGRES_URL: "postgres://default:9OPZaWocGtd4@ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", //env.POSTGRES_URL,
+    //"postgres://default:9OPZaWocGtd4@ep-falling-grass-a46ompk5-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   listPerPage: 10,
   pool: {
     max: 5,
